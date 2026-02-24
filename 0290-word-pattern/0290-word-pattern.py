ab060b8cba3c54f1d8d601704085ffbaf3cb71d0
macreai <76419622+macreai@users.noreply.github.com>
2026-02-24 21:08:04 +0700

    Time: 0 ms (100%), Space: 19.5 MB (25.14%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+
+        my_dict = {}
+        for i in range(len(pattern)):
+            if pattern[i] not in my_dict:
+                if words[i] not in my_dict.values():
+                    my_dict[pattern[i]] = words[i]
+            word = my_dict.get(pattern[i])
+            if word != words[i]:
+                return False 
+        return True
+
+        
